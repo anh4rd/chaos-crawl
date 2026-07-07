53dcb2ce8d56e80f6623c744c511cdc48f37c4f5
Anna Harding <anna@goseetv.co.uk>
2026-07-07 21:16:14 +0100

    Updates

---
+++ assets/Input-CBUQrC2o.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-BdT-bhQb.js";var t=e();function n(e){return(0,t.jsx)(`input`,{...e,className:`w-full rounded-2xl border-4 border-pink-500 bg-white p-4 text-black`})}export{n as t};
